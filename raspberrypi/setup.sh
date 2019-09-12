c1576620aee0db9bc1e68d4db9c22723b4c9a106
Zeroeh Dev <Zeroeh@users.noreply.github.com>
2019-09-12 15:37:16 -0500

    Update setup.sh

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# setup ip forwarding on raspberry pi. (tested & working on stretch. Kernel: 4.19.66-v7+)
-# this script will enable packet forwarding between eth0 and wlan0 (devices connected via ethernet will get internet through the pi's wifi)
-
 if [[ $EUID -ne 0 ]]; then
 	echo "This script requires sudo/root privileges"
 	exit 1
@@ -13,6 +10,3 @@ sudo iptables -t nat -F #flush all existing rules in the nat table
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 sudo iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
-
-echo "If no messages were displayed before this, you're good to go :)"
-
