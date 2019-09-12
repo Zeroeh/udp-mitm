247b64bb51e61cf33d7e51b6da367bdfe6164a33
Zeroeh Dev <Zeroeh@users.noreply.github.com>
2019-09-11 23:30:16 -0500

    Add files via upload

---
+++ test/del_route.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script requires root privileges"
+	exit 1
+fi
+
+# run through twice for the game im testing, but you can change this to be variable based on program args
+sudo iptables -t nat -D PREROUTING 1 && sudo iptables -t nat -D OUTPUT 1
+sudo iptables -t nat -D PREROUTING 1 && sudo iptables -t nat -D OUTPUT 1
+echo "sudo iptables -t nat -D PREROUTING 1 && sudo iptables -t nat -D OUTPUT 1"
